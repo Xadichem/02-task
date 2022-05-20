e6a5c5e6782c8d5516940d674a86071e845f62d2
Gulnur Berdalieva <gulnur.berdalieva.kk@gmail.com>
2022-05-21 01:44:43 +0600

    Add JS/script5.js

--- JS/script5.js
+++ JS/script5.js
@@ -1,11 +1,5 @@
-// let creditCard = prompt("Введите номер карты");
-// number = creditCard.replace(creditCard.slice(0, -4), "**********");
-// alert("Номер карты" + number);
-
 let cardNumber = prompt("Номер вашей карты");
 let last4 = cardNumber.slice(-4);
-let astynkySifra = cardNumber.slice(0, -4);
-let zamena = astynkySifra
-  .replace(astynkySifra, "*")
-  .repeat(astynkySifra.length);
-console.log(`Последние цифры вашей карты: ${zamena + last4}`);
+let creditCard = cardNumber.slice(0, -4);
+let number = creditCard.replace(creditCard, "*").repeat(creditCard.length);
+alert(`Последние цифры вашей карты: ${number + last4}`);
